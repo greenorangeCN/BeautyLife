0dd85409c5bdea098411d572c6ba4cb08213f272
greenorangeCN <42231810@qq.com>
2014-08-02 00:07:34 +0800

    11

---
+++ BeautyLife/StewardFeeView.h
@@ -0,0 +1,13 @@
+//
+//  StewardFeeView.h
+//  BeautyLife
+//
+//  Created by Seven on 14-8-1.
+//  Copyright (c) 2014年 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StewardFeeView : UIViewController
+
+@end
---
+++ BeautyLife/StewardFeeView.m
@@ -0,0 +1,38 @@
+//
+//  StewardFeeView.m
+//  BeautyLife
+//
+//  Created by Seven on 14-8-1.
+//  Copyright (c) 2014年 Seven. All rights reserved.
+//
+
+#import "StewardFeeView.h"
+
+@interface StewardFeeView ()
+
+@end
+
+@implementation StewardFeeView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
